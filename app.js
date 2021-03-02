fcd32f13c749a0274086d4764326357bb23c4f18
GRW <GRWathen@aol.com>
2021-03-02 11:05:55 -0800

    getDatabaseUri()

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
 //app.use(morgan("dev")); // tiny
 //app.use(helmet());
 
--- db.js
+++ db.js
@@ -2,12 +2,11 @@ const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
 const db = new Client({
-    "connectionString": DB_URI,
-    "dialectOptions": {
-        "ssl": {
-            "rejectUnauthorized": false
-        }
-    }
+    connectionString: DB_URI,
+    //connectionString: getDatabaseUri(),
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 //const db = new Client(process.env.DATABASE_URL || "postgresql:///swpagDB");
 
